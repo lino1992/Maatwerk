e2ad6e7b85e66cc492880ab613eb85b79b7a0917
Ruthger_vde <ruthgervdeikhof@hotmail.com>
2015-03-18 13:40:58 +0100

    test

---
+++ Fase 1/Tramverdeelsysteem/Tramverdeelsysteem/Classes/CleaningJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tramverdeelsysteem.Classes
+{
+    class CleaningJob:Job
+    {
+        private int cleaningjobid;
+        private string servicetype;
+
+        public int CleaningJobID { get { return cleaningjobid; } }
+        public string ServiceType { get { return servicetype; } }
+
+        public CleaningJob(int jobid, int trainid, DateTime begindate, DateTime enddate, int cleaningjobid, string servicetype)
+            :base(jobid, trainid, begindate, enddate)
+        {
+            this.cleaningjobid = cleaningjobid;
+            this.servicetype = servicetype;
+        }
+    }
+}
