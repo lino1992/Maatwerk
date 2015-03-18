0779f15074e3ba9e09a6a8254ab9f2520fe1df31
Ruthger_vde <ruthgervdeikhof@hotmail.com>
2015-03-18 14:22:33 +0100

    github

--- Fase 1/Tramverdeelsysteem/Tramverdeelsysteem/Classes/Section.cs
+++ Fase 1/Tramverdeelsysteem/Tramverdeelsysteem/Classes/Section.cs
@@ -8,5 +8,16 @@ namespace Tramverdeelsysteem.Classes
 {
     public class Section
     {
+        private int section;
+        private string blocked;
+
+        public int Section { get { return section; } }
+        public string Blocked { get { return blocked; } }
+
+        public Section(int section, string blocked)
+        {
+            this.section = section;
+            this.blocked = blocked;
+        }
     }
 }
--- Fase 1/Tramverdeelsysteem/Tramverdeelsysteem/Classes/User.cs
+++ Fase 1/Tramverdeelsysteem/Tramverdeelsysteem/Classes/User.cs
@@ -8,5 +8,22 @@ namespace Tramverdeelsysteem.Classes
 {
     public class User
     {
+        private int userid;
+        private string role;
+        private string username;
+        private string password;
+
+        public int UserID { get { return userid; } }
+        public string Role { get { return role; } }
+        public string Username { get { return username; } }
+        public string Password { get { return password; } }
+
+        public User(int userid, string role, string username, string password)
+        {
+            this.userid = userid;
+            this.role = role;
+            this.username = username;
+            this.password = password;
+        }
     }
 }
