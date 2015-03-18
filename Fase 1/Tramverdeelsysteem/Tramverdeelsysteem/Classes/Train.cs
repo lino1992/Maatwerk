67e8718a1736c2a46379f321c913559ac5700cf1
lino1992 <lino_thaencharun@hotmail.com>
2015-03-18 14:45:57 +0100

    update

--- Fase 1/Tramverdeelsysteem/Tramverdeelsysteem/Classes/Train.cs
+++ Fase 1/Tramverdeelsysteem/Tramverdeelsysteem/Classes/Train.cs
@@ -8,5 +8,19 @@ namespace Tramverdeelsysteem.Classes
 {
     public class Train
     {
+        private int trainid;
+        private string status;
+        private DateTime servicedate;
+
+        public int TrainID { get { return trainid; } }
+        public string Status { get { return status; } }
+        public DateTime ServiceDate { get { return servicedate; } }
+
+        public Train(int trainid, string status, DateTime servicedate)
+        {
+            this.trainid = trainid;
+            this.status = status;
+            this.servicedate = servicedate;
+        }
     }
 }
